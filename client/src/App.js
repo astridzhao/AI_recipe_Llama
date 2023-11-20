cf876406ad01badaa56062337b5d4db81ccebaed
AstridZhao <zhao26z@mtholyoke.edu>
2023-11-19 22:36:10 -0500

    add a new UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,61 @@
 // https://github.com/chaoocharles/sppms/blob/master/project-progress/src/App.js
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Routes, Route } from 'react-router-dom';
-import ReactSwitch from 'react-switch';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Homes';
 import Reports from './pages/Reports';
 import Products from './pages/Products';
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react"
-import { Button } from "@chakra-ui/react"
+import TextField from "@mui/material/TextField"
+import Button from '@mui/material/Button';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import CircularProgress from '@mui/material/CircularProgress';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+// import Switch from '@mui/material/Switch';
+// import Switch from 'react-switch'
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Alert from '@mui/material/Alert';
+import Grid from '@mui/material/Grid';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copy from "copy-to-clipboard";
 
 
 function App() {
   const [formValues, setFormValues]  = useState({
-    ingredient: "Tomato, Egg, Cheese, Turkey",
-    cuisine_style: "Asian",
+    ingredient: "Tomato, Egg, Cheese",
+    cuisine_style: "Chinese",
     serving_size: "2",
   });
   const [recipeResponse, setRecipeResponse] = useState(null);
   const [error, setError] = useState(null);
-  const [checked, setChecked] = useState(true);
   const [selectedRestriction, setSelectedRestriction] = useState("No Restriction")
-  const handleChange_recipelength = val => {
-    setChecked(val);
-  }
-  const handleChange_dietary = (event) => {
-    setSelectedRestriction(event.target.value); // onChange handler
-};
-  const change = event =>{
-    const {name, value} = event.target
+
+  const copyToClipboard = () => {
+      copy(recipeResponse);
+      alert(`You have copied the recipe.`);
+  };
+
+  const handleChange_values = event1 =>{
+    const {name, value} = event1.target
     setFormValues(prevValues => ({
       ...prevValues, // Spread the previous values to retain them
       [name]: value  // Update only the input field with the new value
     }));
   }
+  
+  const handleChange_dietary = (event3) => {
+    setSelectedRestriction(event3.target.value); // onChange handler
+  }
+  // const onCopy = () => {
+  //   setIsCopied(true);
+  //   setTimeout(() => setIsCopied(false), 1500); // Reset the copied status after 1.5 seconds
+  // };
 
   function GenerationClick() {
     let data = {
@@ -44,8 +66,7 @@ function App() {
       body: JSON.stringify({'ingredient': formValues.ingredient, 
                             'cuisine_style': formValues.cuisine_style, 
                             'serving_size': formValues.serving_size,
-                            'dietary_restriction': selectedRestriction,
-                            'easy_recipe': checked}),
+                            'dietary_restriction': selectedRestriction}),
     };
 
     //outputs a message to the web console
@@ -74,9 +95,9 @@ function App() {
     setError(" ")
   }
 
- return (
+return (
       <div className="App">
-        <>
+          <>
           <Router>
             <Navbar />
             <Routes>
@@ -87,62 +108,80 @@ function App() {
           </Router>
         </>
 
+        <Alert severity="info">The estimated waiting time is 40s.</Alert>
+        <Alert severity="warning">Please empty the generation before start a new generation.</Alert>
+
         <div className="App-header">
-        <p>Enter your recipe ingredients, cuisine style, and serving size! The estimated waiting time is 40s.</p>
+          
           <div className="input-box">
-              <input className="ingredient-box"
+          <Grid container spacing={2}>
+            <Grid item xs={12} >
+              <TextField fullWidth label="Ingredients" variant="outlined" 
+                className="ingredient-box"
                 name="ingredient" // Correct attribute name is "name"
                 value={formValues.ingredient} // Correct attribute name is "value"
-                onChange={change} 
+                onChange={handleChange_values} 
                 type="text" 
                 required />
-              <input className="cuisine-box"
-                name="cuisine_style" // Correct attribute name is "name"
-                value={formValues.cuisine_style} // Correct attribute name is "value"
-                onChange={change} 
-                type="text"
-                required/>
-              <input className="serving-box"
-                name="serving_size" // Correct attribute name is "name"
-                value={formValues.serving_size} // Correct attribute name is "value"
-                onChange={change} 
-                type = "number"
-                required/>
-              <div className="recipeLength" style={{textAlign: "center"}}>
-                    <p style={{ marginRight: "10px" }}>Easy Recipe</p>
-                    <ReactSwitch
-                      checked={checked}
-                      onChange={handleChange_recipelength}
-                      required
-                    />
-              </div>
-              <div className="dietaryRestriction" style={{textAlign: "center", font: "Times New Roman"}}>
-                    <p style={{ marginRight: "10px" }}>Dietary Restriction</p>
-                    <select value={selectedRestriction}  onChange={handleChange_dietary}> {/* Select component with value and onChange */}
-                      <option value="No Restriction">No Restriction</option> 
-                      <option value="Gluten-Free">Gluten-Free</option> 
-                      <option value="Vegetarian">Vegetarian</option>
-                      <option value="Pescatarian">Pescatarian</option>
-                      <option value="Vegan">Vegan</option>
-                      <option value="Lactose Intolerance">Lactose Intolerance</option>
-                      <option value="Shellfish Allergies">Shellfish Allergies</option>
-                      <option value="Nut Allergies">Nut Allergies</option>
-                      <option value="Low-Carb/Keto">Low-Carb/Keto</option>
-                      
-                  </select>
-              </div>
-
+            </Grid>
+            <Grid item xs={4}>
+              <TextField fullWidth id="Required-basic" label="Serving Size" variant="outlined" 
+                  className="serving-box"
+                  name="serving_size" // Correct attribute name is "name"
+                  value={formValues.serving_size} // Correct attribute name is "value"
+                  onChange={handleChange_values} 
+                  type = "number"
+                  required/>
+            </Grid>
+            <Grid item xs={4} >
+              <TextField  fullWidth
+                  id="Required-basic" label="Cuisine" variant="outlined" 
+                  className="cuisine-box"
+                  name="cuisine_style" 
+                  value={formValues.cuisine_style} 
+                  onChange={handleChange_values} 
+                  type="text"
+                  required/>
+            </Grid>
+            
+            <Grid item xs={4}>
+              <section className="dietaryRestriction" style={{textAlign: "left", font: "Times New Roman"}}>
+                  <FormControl fullWidth sx={{minWidth: 200 }} >
+                  <InputLabel id="Required-basic" >Restriction</InputLabel>
+                      < Select 
+                        value={selectedRestriction}  
+                        label="Required" 
+                        labelId="Required-basic"
+                        id = "Required-basic"
+                        name = "dietary_restriction"
+                        onChange={handleChange_dietary} required> {/* Select component with value and onChange */}
+                        <MenuItem value={"No Restriction"}>No Restriction</MenuItem> 
+                        <MenuItem value={"Gluten-Free"}>Gluten-Free</MenuItem> 
+                        <MenuItem value={"Vegetarian"}>Vegetarian</MenuItem>
+                        <MenuItem value={"Pescatarian"}>Pescatarian</MenuItem>
+                        <MenuItem value={"Vegan"}>Vegan</MenuItem>
+                        <MenuItem value={"Lactose Intolerance"}>Lactose Intolerance</MenuItem>
+                        <MenuItem value={"Shellfish Allergies"}>Shellfish Allergies</MenuItem>
+                        <MenuItem value={"Nut Allergies"}>Nut Allergies</MenuItem>
+                        <MenuItem value={"Low-Carb/Keto"}>Low-Carb/Keto</MenuItem>
+                      </Select>
+                  </FormControl> 
+                </section>
+              </Grid>
+            </Grid>
           </div>
 
+        
           <div className="button">
           <Button className='button-submit'  onClick={GenerationClick}>Recipe start</Button>
           <Button className = "empty-button" onClick={EmptyClick}>Empty Generation </Button>
+          <Button className='copy-button' onClick={copyToClipboard}>Copy to Clipboard</Button>
           </div>
 
-         <div className="recipe-generation-output"  style={{ display: "inline-block" , width: "1200px",
-                height: "380px", color:"white",  borderBlock: "solid", borderBlockColor: "white", writingMode: "horizontal-tb"
-                }}> {recipeResponse }{error}  
-                
+          <div className="recipe-generation-output"  style={{ display: "inline-block" , width: "1200px",
+                height: "490px", padding: "20px", color:"black",  backgroundColor: "rgb(243, 250, 224)", 
+                borderBlock: "solid", borderBlockColor: "white", writingMode: "horizontal-tb",
+                }}> {recipeResponse }{error} 
           </div>
 
       </div>
@@ -150,3 +189,20 @@ function App() {
     );
 }
 export default App;
+
+
+// <div className="recipeLength">
+// <FormControl component="fieldset">
+//   <FormControlLabel
+//     value="Easy recipe"
+//     control={
+//       <Switch
+//         checked={checked}
+//         inputProps={{ 'aria-label': 'Switch' }}
+//         onChange={handleChange_recipelength}
+//       />
+//     }
+//     label="Easy recipe"
+//     labelPlacement="top"
+//   />
+// </FormControl></div>
--- flask/server.py
+++ flask/server.py
@@ -23,7 +23,6 @@ recipe_input = api.model('Recipe', {
     'ingredient': fields.String( description='The ingredient list'),
     'serving_size': fields.Integer(description='The serving size setting'),
     'dietary_restriction': fields.String(description='The dietary restriction setting'),
-    'recipe_length': fields.Boolean(description='The recipe length setting')
 })
 
 # Specifies a response format:
@@ -33,44 +32,29 @@ recipe_response = api.model('RecipeResponse', {
 
 class recipeDAO(object):    
     def generate(self, request):
-    #get user input by using request
-        # serving_size = 2
-        # cuisine_style = "Asian"
-        # user_input = request['ingredient']
-        
         # Extract user inputs from the request dictionary
         user_input = request.get('ingredient')  # Provide a default value if key is not found
         cuisine_style = request.get('cuisine_style')  # Default to 'Asian' if not provided
         serving_size = request.get('serving_size')  # Default to 2 if not provided
         dietary_restriction = request.get("dietary_restriction")
-        easy_recipe = request.get("easy_recipe")
-
-        if easy_recipe == True:
-            length = 60
-        else:
-            length = 90
         
         #initialize the dialog
         # 
         SYSTEM_PROMPT = f"""You are a helpful recipe-generating assistant. Based on the user provided ingredients, you will generate a recipe. \
-        Make sure to follow the rules: \
-        1. Make the instruction description step by step in {length} words. \
-        2. If you will need to use any ingredients that the user does not provide, tell the user. \
-        3. The serving size is {serving_size}. The cuisine style is {cuisine_style}. \
-        4. The default type of dish is airfry/oven/stir-fry, unless the user specifies.
-        5. Notice the user has {dietary_restriction} restriction."""
+        Adjust recipe by following the rules: \
+        1. Describe Ingredient List, Instruction in 150 words. \
+        2. If you need add any other ingredients, tell the user. \
+        3. Serving size is for {serving_size} adults. Cuisine style is {cuisine_style}. \
+        4. Notice the user has {dietary_restriction} restriction."""
         # 6. You don't have to use all ingredients.
         # 3. Choose some common spice/sauce. \
         # 4. Provide information about the recipe such as kitchen utensils, preparation steps. 
-        
-        #Testing: 2nd version
-        # SYSTEM_PROMPT ="You are a helpful recipe-generating assistant."
         
         dialog_history = [{"role": "system", "content": SYSTEM_PROMPT}]
         dialog_history.append({"role": "user", "content": user_input})
         
         #Testing: 2nd version
-        prompt = f"I am very new to cook. Use {user_input}, generate a detailed recipe description, including kitchen utensil, cooking methods, etc. Make the description about {length} words. Print the recipe in a nice format. Choose some common spice/sauce first. The serving size is {serving_size}. The cuisine style is {cuisine_style}. "
+        # prompt = f"I am very new to cook. Use {user_input}, generate a detailed recipe description, including kitchen utensil, cooking methods, etc. Make the description about {length} words. Print the recipe in a nice format. Choose some common spice/sauce first. The serving size is {serving_size}. The cuisine style is {cuisine_style}. "
         # prompt = f"As a beginner in cooking, I need a detailed recipe using {user_input}.Please include kitchen utensils, how much ingredients I need to use, cooking methods, and a nicely formatted recipe. Aim for in {length} words. Start with common spices or sauces. The serving size should be for {serving_size} people, and the cuisine style is {cuisine_style}.I would appreciate detailed instructions on how to prepare each dish, including cooking time."
 
         llama_cpp_path = "/Users/astridz/Documents/AI_recipe/llama.cpp"
@@ -84,28 +68,28 @@ class recipeDAO(object):
         B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
         SYSTEM_PROMPT = B_SYS + SYSTEM_PROMPT + E_SYS
         template = B_INST + SYSTEM_PROMPT + user_input + E_INST
-        # assistant_response = prompt
-        # response = {'llm_output': assistant_response}
         
+        # ./main -m llama-2-7b-chat.Q4_K_M.gguf -c 2048 -b 1024 -p "<<SYS>you are a helpful recipe-generate assistant\n<</SYS>>\n\n"
+        
+        args = ['./main', '-m', pure_name, '-c', '2048', '-n', '1024', '-b', '1024', '-p', template]
+
         if (os.getcwd() != llama_cpp_path):
             os.chdir(llama_cpp_path)
-            
-        args = ['./main', '-m', pure_name, '-c', '2048', '-n', '1024', '-b', '1024', '-ngl', '48', '-p', template]
+        
         process = subprocess.Popen(args, stdout=subprocess.PIPE, text=True)
         start_time = time.time()
         output = process.stdout.read()
-        # assistant_response = "".join(output)
-        # print(assistant_response)
+        # Testing:
+        # dialog_history.append({"role": "assistant", "content": output})
+        # response = {'llm_output': output}
+
         marker_index = output.find("[/INST]")
+        assistant_response = ""
         if marker_index != -1:
             assistant_response = output[marker_index + len("[/INST]"):] 
-            dialog_history.append({"role": "assistant", "content": assistant_response})
-            # assistant_response = user_input +"\n new recipe"
-            response = {'llm_output': assistant_response}
-            
-        # else: response = {'llm_output': output}
-        #Testing: 2nd version   
-        # response = {'llm_output': assistant_response}
+        dialog_history.append({"role": "assistant", "content": assistant_response})
+        response = {'llm_output': assistant_response}
+
         end_time = time.time()
         elapsed_time = end_time - start_time
         print("total time :",  elapsed_time)
@@ -126,32 +110,6 @@ class TodoList(Resource):
     def post(self):
         '''Create a new task'''
         return DAO.generate(api.payload), 201
-
-# # { "ingredient" : "tomato and egg"}
-# def generate_text(dialog_history, q, process: str) -> str:
-
-#     while True:
-#         output = process.stdout.readline()
-        
-#         if output == '' and process.poll() is not None:
-#             print("Subprocess has completed.")
-#             break
-        
-#         if output:
-#             assistant_response = output.strip()
-#             q.put(assistant_response)
-#             dialog_history.append({"role": "assistant", "content": assistant_response})
-        
-            # if '[INST]' in output or '<>' in output:
-            #     continue 
-            
-            # marker_index = output.find("[/INST]")
-            # if marker_index != -1:
-            #     assistant_response = output[marker_index + len("[/INST]"):]
-            
-            # print(assistant_response)
-        #     dialog_history.append({"role": "assistant", "content": assistant_response})
-        # return assistant_response
             
 if __name__ == "__main__":
     app.run(debug=True)
